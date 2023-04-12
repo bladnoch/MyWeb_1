4b00c4e6a56a226e76324a83e0898ccfd62c2572
DK <dongwook5478@gmail.com>
2023-04-12 20:51:18 +0900

    add object

--- studyFunction.js
+++ studyFunction.js
@@ -1,10 +1,22 @@
 // console.log("hello");
 
-function sayHello(inName){
-    let name=inName;
-    console.log("hello my name is",name);
+// #1
+// function sayHello(inName,age){
+//     let name=inName;
+//     let ag=age;
+//     console.log("hello my name is",name,"I am",ag);
+// }
+//
+// // alert(sayHello());
+// sayHello("Daniel",29);
+
+
+// #2
+const player={ //object 안에 function을 만들 수 있다.
+    name:"Daniel",
+    sayHello: function(otherPersonsName){
+        console.log("hello", otherPersonsName);
+    }
 }
-
-// alert(sayHello());
-sayHello("Daniel");
-
+console.log(player.name);
+player.sayHello("DK");

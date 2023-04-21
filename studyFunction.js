6140645707e004e4dae4e0977f4512048c7dc692
DK <dongwook5478@gmail.com>
2023-04-21 21:41:46 +0900

    update studyFunction.js

--- studyFunction.js
+++ studyFunction.js
@@ -11,45 +11,51 @@
 // sayHello("Daniel",29);
 
 
-// #2
-const player={ //object 안에 function을 만들 수 있다.
-    name:"Daniel",
-    sayHello: function(otherPersonsName){
-        console.log("hello", otherPersonsName);
+// // #2
+// const player={ //object 안에 function을 만들 수 있다.
+//     name:"Daniel",
+//     sayHello: function(otherPersonsName){
+//         console.log("hello", otherPersonsName);
+//     }
+// }
+// console.log(player.name);
+// player.sayHello("DK");
+//
+//
+// const names ={
+//     name: "Daniel",
+//     age: 9,
+//     height: 171,
+// };
+// console.log(names.name);
+// console.log(names.height);
+//
+// names.name="Dounguk";
+// console.log(names.name);
+// const count=["name", "age","height"];
+
+// const age=94;
+// function calculateKrAge(ageOfForeigner){
+//     ageOfForeigner+=2;
+//     return ageOfForeigner;
+// }
+//
+// console.log(calculateKrAge(age));
+
+const Calc={
+    add: function (a,b){
+        return a+b;
+    },
+    subt: function (a,b){
+        return a-b
+    },
+    times: function(a,b){
+        return a**b
+    },
+    div:function (a,b){
+        return a/b
     }
 }
-console.log(player.name);
-player.sayHello("DK");
 
-
-const names ={
-    name: "Daniel",
-    age: 9,
-    height: 171,
-};
-console.log(names.name);
-console.log(names.height);
-
-names.name="Dounguk";
-console.log(names.name);
-const count=["name", "age","height"];
-
-
-const calculator={
-    add: function(a,b){
-        console.log("add ",a+b);
-    },
-    subtract: function (a,b){
-        console.log("subtract ",a-b);
-    },
-    divide: function (a,b){
-        console.log("divide ",a/b);
-    },
-    times: function (a,b){
-        console.log("times ", a**b);
-    }
-}
-calculator.add(1,2);
-calculator.subtract(1,2)
-calculator.divide(1,2)
-calculator.times(1,2)
+const added=Calc.add(1,5);
+console.log(Calc.subt(12,added));

48fc02ab384a0dd8f6e859ef21a655545641c34c
DK <dongwook5478@gmail.com>
2023-07-01 20:24:16 +0900

    update 3.3

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const title =document.querySelector(".hello h1");
+const title =document.querySelector(".hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    if(title.style.color=="blue"){
+        title.style.color="pink"
+    }
+    else {
+        title.style.color="blue"
+    }
+
+}
+
+
+
+title.addEventListener("click",handleTitleClick);
+
+

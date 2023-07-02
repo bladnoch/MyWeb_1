c32416fde4f9b80d1a2ccc9ae84b0f6619cb11e6
DK <dongwook5478@gmail.com>
2023-07-02 20:05:02 +0900

    add style.css

--- app.js
+++ app.js
@@ -2,27 +2,14 @@ const h1 =document.querySelector("div.hello:first-child h1");
 
 
 
-// function handleTitleClick(){
-//     let currentColor=h1.style.color;
-//     console.log(h1.style.color) //getter
-//     h1.style.color="blue"; //setter
-//     console.log(h1.style.color);
-//
-// }
-h1.addEventListener("click", handleTitleClick);
 
 
 function handleTitleClick(){
-    const currentColor=h1.style.color;
-    let newColor;
-    if(currentColor==="blue"){
-        newColor="pink"
-    }
-    else {
-        newColor="blue"
-    }
-    h1.style.color=newColor;
+
 }
+
+h1.addEventListener("click", handleTitleClick);
+
 
 //
 // function handleMouseEnter(){

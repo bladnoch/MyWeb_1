2d0630e20d8f48f3b8bde928282337391d1d687b
DK <dongwook5478@gmail.com>
2023-07-03 20:02:25 +0900

    update before 4.0

--- app.js
+++ app.js
@@ -7,36 +7,3 @@ function handleTitleClick(){
 }
 
 h1.addEventListener("click", handleTitleClick);
-
-
-//
-// function handleMouseEnter(){
-//     h1.innerText="yes mouse";
-// }
-//
-// function handleMouseLeave(){
-//     h1.innerText="no mouse";
-// }
-// function handleWindowResize(){
-//     document.body.style.backgroundColor="tomato";
-// }
-//
-// function hadleWindowCopy(){
-//     alert("copied");
-// }
-//
-// function hadleWindowOffline(){
-//     alert("no WI-FI");
-// }
-//
-// function handleWindowOnline(){₩
-//     alert("yes wi-fi");
-// }
-//
-// h1.addEventListener("mouseenter",handleMouseEnter);
-// h1.addEventListener("mouseleave",handleMouseLeave);
-//
-// window.addEventListener("resize",handleWindowResize);
-// window.addEventListener("copy",hadleWindowCopy);
-// window.addEventListener("offline",hadleWindowOffline);
-// window.addEventListener("online",handleWindowOnline);

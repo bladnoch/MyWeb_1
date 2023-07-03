b5790cd0603f688d1bca15ca7c6fc0536ce5ccb5
DK <dongwook5478@gmail.com>
2023-07-03 21:44:01 +0900

    update 4.4

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 
 const loginInput= document.querySelector("#login-form input");
 const loginButton= document.querySelector("#login-form button");
-const loginForm=document.querySelector("#login-form")
-
+const loginForm=document.querySelector("#login-form");
+const greeting =document.querySelector("#greeting");
 const link =document.querySelector("a");
+
+const HIDDEN_CLASSNAME="hidden";
 
 function onLogSubmit(event){
     event.preventDefault();
-    console.log(event);
+    const username=loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 function handleLinkClick(event){
     event.preventDefault();
-
 }
 
 loginForm.addEventListener("submit",onLogSubmit);

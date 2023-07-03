d4ce4519f819df4cd7efb0ea5b37446c02ce7ba0
DK <dongwook5478@gmail.com>
2023-07-03 20:32:09 +0900

    update 4.1

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const loginButton= document.querySelector("#login-form button");
 
 
 function onLogBtnClick(){
-    console.dir(loginInput.value);
-    console.log("click!!");
+    const value=loginInput.value;
+    console.log(value);
+    // if(value===""){
+    //     alert("please put id");
+    // } else if(value.length>15){
+    //     alert("your name is too long")
+    // } else if(value.length<8){
+    //     alert("your name is too short")
+    // }
+
 }
 
 loginButton.addEventListener("click", onLogBtnClick);

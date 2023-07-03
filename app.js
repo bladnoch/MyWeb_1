847a2e27e1effa40a0b0430b9af98e4b649b5c22
DK <dongwook5478@gmail.com>
2023-07-03 21:20:05 +0900

    update 4.4

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const loginInput= document.querySelector("#login-form input");
 const loginButton= document.querySelector("#login-form button");
 const loginForm=document.querySelector("#login-form")
 
+const link =document.querySelector("a");
+
 function onLogSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    console.log(event);
+}
+function handleLinkClick(event){
+    event.preventDefault();
+
 }
 
 loginForm.addEventListener("submit",onLogSubmit);
-
+link.addEventListener("click",handleLinkClick);

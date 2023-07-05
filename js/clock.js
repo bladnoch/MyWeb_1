3b556ddf4e2b74f6872038678f520360b0f76235
DK <dongwook5478@gmail.com>
2023-07-05 23:39:20 +0900

    update clock format

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,10 @@ count=0;
 function getClock(){
 
     const date= new Date();
-    // console.log(`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`)
-    clock.innerText=`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
+    const hrs=String(date.getHours()).padStart(2,"0");
+    const mins=String(date.getMinutes()).padStart(2,"0")
+    const secs =String(date.getSeconds()).padStart(2,"0")
+    clock.innerText=`${hrs} : ${mins} : ${secs}`
 }
 
 getClock();

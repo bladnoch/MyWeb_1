2e7b5e66abba9079d5e4c76fe9c0de8990993bda
DK <dongwook5478@gmail.com>
2023-07-09 01:53:28 +0900

    update todo

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,11 @@
+
 /**
  * clock.js: display time on website
  * @author DoungukKim
  * @since 7/6/2023
  * @version v0.0.1
  */
+
 
 const clock=document.querySelector("h2#clock");
 
--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,11 @@
+
+/**
+ * todo.js: display todo list on the screen
+ * @author DoungukKim
+ * @since 7/9/2023
+ * @version v0.0.1
+ */
+
 const toDoForm=document.getElementById("todo-form");
 const toDoInput=toDoForm.querySelector("#todo-form input");
 const toDoList=document.getElementById("todo-list");
@@ -5,10 +13,20 @@ const toDoList=document.getElementById("todo-list");
 
 
 function handleToDoSubmit(){
+    // event.stopImmediatePropagation();
     event.preventDefault();
     const newTodo=toDoInput.value;
-    console.log(toDoInput.value);
     toDoInput.value="";
+    paintTodo(newTodo);
 }
+
+function paintTodo(newTodo){
+    const li=document.createElement("li");
+    const span=document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
